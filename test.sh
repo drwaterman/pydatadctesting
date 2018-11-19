f8194dae8a21faab121ccae8e0e115aeb655a9d8
davidwaterman <david.waterman@bytecubed.com>
2018-11-19 10:10:47 -0500

    Replaced test.sh with pytest.ini - thanks to audience comment from PyDataDC

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ ! -d tests/test-logs ]
-then
-    mkdir tests/test-logs
-fi
-
-pytest --html=tests/test-logs/testreport.html --self-contained-html \
---cov=experiment --cov-report term-missing -r aPp tests
